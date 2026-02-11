064ea2cc1b7491f2a6cecd64f2921445ca828777
ryota-mukeikaku <moriyama_ryota@mukeikaku.co.jp>
2026-02-12 08:05:34 +0900

    Modify

--- src/component/Location.tsx
+++ src/component/Location.tsx
@@ -77,8 +77,13 @@ function bearing(from: Point, to: Point): number {
 }
 
 function bearingToDirection(deg: number): string {
-  const dirs = ["北", "北東", "東", "南東", "南", "南西", "西", "北西"];
-  const idx = Math.round(deg / 45) % 8;
+  const dirs = [
+    "北", "北北東", "北東", "東北東",
+    "東", "東南東", "南東", "南南東",
+    "南", "南南西", "南西", "西南西",
+    "西", "西北西", "北西", "北北西",
+  ];
+  const idx = Math.round(deg / 22.5) % 16;
   return dirs[idx];
 }
 
